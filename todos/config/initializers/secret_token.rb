fd647d2274e1a4eb50168456dcf700922eefbd6e
Allen Leis <allen.leis@threespot.com>
2012-09-09 23:41:48 -0400

    initial import: blank rails app

---
+++ todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'dcfee0c8e9eb7d7504c5a3be74bc144c5670c7ae1df20381612ac67670b076b9f5d701f2f4fa38a7139de41d0c3eb9c27d5b989955656277d75296ae70aaa8f3'
